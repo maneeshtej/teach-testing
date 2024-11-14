d0538f510b9af1cf6d810a67cc8ab52ab6cd7a13
maneeshtej <tejsaimaneeesh@gmail.com>
2024-11-14 23:33:48 +0530

    made header added logic to get user name added local storage

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,19 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import './home.css'
+import { supabase } from '../utils/supabase';
 
 function Home() {
     const navigate = useNavigate();
+    const [user, setUser] = useState('');
+    const useremail = Cookies.get('email');
+    
+    useEffect(() => {
+        const getUser = async () => {
+            const { data, error } = await supabase
+                .from('Teachers')
+                .select('name')
+                .eq('email', useremail)
+                .single(); 
+
+            if (error) {
+                console.log("Error fetching data:", error);
+                return;
+            }
+
+            if (data) {
+                Cookies.remove('email');
+                localStorage.setItem('username', data.name);
+            } else {
+                console.log("No user found with this email");
+            }
+        };
+
+        const username = localStorage.getItem('username')
+
+        if (useremail && !username) {
+            getUser();
+        } 
+
+        if (username) {
+            setUser(username);
+        }
+
+    }, [useremail]);
 
     const handleLogOut = () => {
         Cookies.remove('auth_token');
+        localStorage.removeItem('username');
             navigate('/');
     }
   return (
-    <div style={{
-        cursor: 'pointer',
-    }} 
-    onClick={handleLogOut}>Log Out</div>
+    <div className='container'>
+        <div className='header'>
+            <div className='hero'><strong>HELLO </strong>, {user}</div>
+            <div className='menu'>
+                Menu
+            </div>
+        </div>
+    <button onClick={handleLogOut}>Log Out</button>
+    </div>
   )
 }
 
