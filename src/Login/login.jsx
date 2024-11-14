e3a62540715124bca6814a9f64416dbe8d9ac85d
Sankara Kala <varmaneesh@Sankaras-MacBook-Air.local>
2024-11-14 14:53:46 +0530

    created src/Home/Home.jsx created src/Login/Login.jsx created src/Login/singup.jsx added supabase auth to login (working)

---
+++ src/Login/login.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { supabase } from '../utils/supabase';
+import { Link, useNavigate } from 'react-router-dom';
+import "./login.css"
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+
+
+//   console.log("supabase : ", supabase)
+
+  const handleSubmit = async (login) => {
+    login.preventDefault();
+    setLoading(true);
+
+    
+
+    try {
+      const { user, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) throw error;
+      console.log('Logged in user:', user);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      console.log('Error:', err.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Email:</h1>
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <h1>Password:</h1>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input type="submit" value={loading ? 'Logging in...' : 'Submit'} disabled={loading} />
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <Link className='sign-up' to={"/signup"}>Sign Up</Link>
+    </>
+  );
+}
+
+export default Login;
