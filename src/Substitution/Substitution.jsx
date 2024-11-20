29d1a1837417ccc1ada52dadb0e2a41035acc944
maneeshtej <tejsaimaneeesh@gmail.com>
2024-11-20 12:05:49 +0530

    added new folder Substitution created new file subsitution created new file Calender.jsx added calender logic

---
+++ src/Substitution/Substitution.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Calendar from './components/Calender'
+
+function Substitution() {
+  return (
+    <div>
+        <Calendar></Calendar>
+    </div>
+  )
+}
+
+export default Substitution
---
+++ src/Substitution/components/Calender.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './calender.css'
+
+const Calendar = ({handleSelectedDays}) => {
+    const [currentDate, setCurrentDate] = useState(new Date());
+  
+    const handlePrevMonth = () => {
+        setCurrentDate(new Date(currentDate.setMonth(currentDate.getMonth() - 1)));
+    };
+
+    const handleNextMonth = () => {
+        setCurrentDate(new Date(currentDate.setMonth(currentDate.getMonth() + 1)));
+    };
+
+    const getDaysInMonth = () => {
+        const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+        const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+        const daysInMonth = [];
+        const firstDayOfWeek = firstDayOfMonth.getDay();
+        const totalDays = lastDayOfMonth.getDate();
+
+        for (let i = 0; i < firstDayOfWeek; i++) {
+            daysInMonth.push(null);
+        }
+
+        for (let i = 1; i <= totalDays; i++) {
+            daysInMonth.push(i);
+        }
+
+        return daysInMonth;
+    };
+
+    const daysInMonth = getDaysInMonth();
+
+    const months = [
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ];
+
+    const monthName = months[currentDate.getMonth()];
+    const year = currentDate.getFullYear();
+
+    return (
+        <div>
+            <div className="calendar-header">
+                <button onClick={handlePrevMonth}>Previous</button>
+                <h2>{monthName} {year}</h2>
+                <button onClick={handleNextMonth}>Next</button>
+            </div>
+            <div className="calendar">
+                <div className="days-of-week">
+                    {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day, index) => (
+                        <div key={index}  className="day-name">{day}</div>
+                    ))}
+                </div>
+                <div className="days-grid">
+                    {daysInMonth.map((day, index) => (
+                        <div key={index} className="day">
+                            {day ? day : ''}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Calendar;
